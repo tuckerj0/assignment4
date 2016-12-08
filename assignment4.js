d948d6fb842939915efa3c3c250b60ad7f93e508
tuckerj0 <jat134@pitt.edu>
2016-12-08 18:20:31 -0500

    Update assignment4.js

--- assignment4.js
+++ assignment4.js
@@ -11,7 +11,66 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
-(function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
-})();
+var $all = new Array();
+
+$(document ).ready(function() {
+
+  $.ajax({
+      async: false,
+      type: "GET",
+	  url: 'http://www.mattbowytz.com/simple_api.json?data=interests',
+	  dataType:'json',
+	  type: "GET",
+	  success: function(data){
+		$all = $.merge(data.data, $all);
+	  }
+  });
+    $.ajax({
+      async: false,
+      type: "GET",
+	  url: 'http://www.mattbowytz.com/simple_api.json?data=programming',
+	  dataType: 'json',
+	  success: function(data){
+		  $all = $.merge(data.data, $all);
+
+	  }
+  });
+    $.ajax({
+      async: false,
+      type: "GET",
+	  url: 'http://www.mattbowytz.com/simple_api.json?data=comics',
+	  dataType: 'json',
+	  success: function(data){
+		  $all = $.merge(data.data, $all);
+	  }
+  });
+    console.log($all);
+});
+
+$(".flexsearch-input").keyup(function(event){
+   jQuery('#output').html('');
+  var i =0;
+  var j = 0;
+  var $results = new Array();
+  var $input = $(".flexsearch-input").val().toLowerCase();
+  var $temp = 0;
+
+  if($input.length==0){
+    $("#output").hide();
+    return;
+  }
+  for(i = 0; i < $all.length; i++){
+    $temp = $all[i].toLowerCase();
+    if($temp.substring(0,$input.length)==$input){
+      $results.push($all[i]);
+      j++;
+    }
+  }
+  for(i = 0; i < j; i++){
+       var $line = $results.pop();
+      $temp = 'https://www.google.com/#q=' + $line;
+      $('#output').append('<li><a target="_blank" href="http://www.google.com/search?q=' + $line + '">' + $line + '</a></li>')
+      $("#output").show();
+  }
+
+});
